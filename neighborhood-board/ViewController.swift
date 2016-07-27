dd66d22de2d9c7645add02878700506e16451c60
Andy Kolean <ajkolean@umich.edu>
2016-07-27 17:02:18 -0400

    Initial Commit

---
+++ neighborhood-board/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  neighborhood-board
+//
+//  Created by admin on 7/27/16.
+//  Copyright © 2016 ajkolean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
